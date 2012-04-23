1698a0b5bfc190d969412d10ed5a7a257459c9c0
macarthur <macarthur@sager-laptop.home-network>
2012-04-23 08:28:47 -0400

    changed various files to make them comply with -Wall as much as possible.

--- functions.h
+++ functions.h
@@ -1,7 +1,28 @@
-/*
-* Copyright 2011 Macarthur Inbody
+/*!
+*    Various functions that I have written or have came across to make my life easier.
+*    
+*     Copyright 2011, Macarthur Inbody
+*    
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the Lesser GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 *
-* This code is licensed under the LGPLv3. It is not dual licensed.
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the Lesser GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
+*     
+*   2011-06-20 06:03:57 PM -0400
+*    
+*   These functions work by any input that is provided. For a function demonstrating it.
+*    Please look at the second source file at the post of the question on stack overflow.
+*    It also includes an answer for implenting it using recursion if that is your favored
+*    way of doing it. I personally do not feel comfortable working with recursion so that is
+*    why I went with the implementation that I have included.
 */
 
 /*
@@ -17,8 +38,11 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <assert.h>
 #include <string.h>
+
 #include "math.c"
 #include "string.c"
-
-
+#include "uber_rand.c"
